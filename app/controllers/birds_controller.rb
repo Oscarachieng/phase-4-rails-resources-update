36666d874b132984d55a12dc545a3602cd5dc544
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-09-27 16:18:01 +0300

    commit tree d1ebf19f379116765952b5c08f0b088ea293e952

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,33 @@ class BirdsController < ApplicationController
     end
   end
 
+  # update action
+  def update 
+    @plant_bird = Bird.find_by(id: params[:id])
+    if @plant_bird 
+      @plant_bird.update(bird_params)
+      render json: @plant_bird
+    else
+      render json: {error: "Resource not found"}, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
+end
+
+class Birds::LikesController < ApplicationController
+  def increment_likes
+    @bird_to_update = Bird.find_by(id: params[:id])
+    if @bird_to_update
+      @bird_to_update.update(likes: @bird_to_update.lkes + 1)
+      render json: @bird_to_update
+    else
+      render json: {error: "Resource not found"}, status: :not_found
+    end
+  end
 end
